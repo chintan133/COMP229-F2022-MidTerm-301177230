55c93892aeca1b0d565c8d01fc4c517765f3fa0e
chintan133 <75980405+chintan133@users.noreply.github.com>
2022-10-28 13:54:45 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
   URI: "mongodb://127.0.0.1/faculty_info",
+  "URI": "mongodb+srv://chintan:Cj60Ojm2sbVdBCrV@cluster0.pzwlf.mobodb.net/faculty_Id?retryWrites=true&w=majority"
 };
